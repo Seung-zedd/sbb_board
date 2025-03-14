e4e32b11bcbb0169a843a0e8f4bd45bda50178aa
seung-je <csz0209@gmail.com>
2025-03-14 21:05:58 +0900

    ✨feat: make basic controller and learn how to use lombok

---
+++ src/main/java/com/mysite/sbb/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World SBB";
+    }
+
+    @GetMapping("/jump")
+    public String jump() {
+        return "점프 투 스프링 부트";
+    }
+}
