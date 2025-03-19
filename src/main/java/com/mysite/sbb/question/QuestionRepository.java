f176b14daa7ec50cd4de171f97314cb3d8b60c23
seung-je <csz0209@gmail.com>
2025-03-19 17:43:22 +0900

    ✨feat: add QuestionService and detail page

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     // findById만 기본적으로 제공해주므로 새로 작성
-    Question findBySubject(String subject);
+//    Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
 }
