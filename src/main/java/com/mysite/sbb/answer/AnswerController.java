f60d0c142725299a17e03ee4c7ef521bf0319a7a
seung-je <csz0209@gmail.com>
2025-03-20 13:10:59 +0900

    ✨feat: add AnswerController, AnswerService and answerList view

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@Controller
+@RequiredArgsConstructor
+public class AnswerController {
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(@PathVariable("id") Long id, @RequestParam("content") String content) {
+        Question question = questionService.getQuestion(id);
+        answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
